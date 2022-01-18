0de28c17b10740adfa357663744ae3a22563e6ba
baddar999 <baddar.rehmat@gmail.com>
2022-01-18 22:30:02 +0500

    Completed module

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,2 +1,16 @@
 import mongoose from 'mongoose';
-import { productsSchema } from '../models';
+import { productSchema } from '../models/models';
+
+const Product = mongoose.model('Product', productSchema);
+
+export const addnewProduct = (req, res) => {
+  let newProduct = new Product(req.body);
+
+  newProduct.save((error, Product) => {
+    if (error) {
+      res.send(error);
+    }
+
+    res.json(Product);
+  });
+};
--- src/models/models.js
+++ src/models/models.js
@@ -1,10 +1,23 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-export const productsSchema = new Schema({
-  name: String, // String is shorthand for {type: String}
-  description: String,
-  category: String,
-  price: Number,
-  created_date: { type: Date, default: Date.now },
+export const productSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Enter a product name',
+  },
+  description: {
+    type: String,
+    required: 'Enter a product description',
+  },
+  category: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+  },
 });
