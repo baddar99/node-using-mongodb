2009610c7943ec6e0c0b299b6b6d7ec3dcceaff1
baddar999 <baddar.rehmat@gmail.com>
2022-01-18 22:53:41 +0500

    Completed module

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -6,9 +6,9 @@ const Product = mongoose.model('Product', productSchema);
 export const addnewProduct = (req, res) => {
   let newProduct = new Product(req.body);
 
-  newProduct.save((error, Product) => {
+  newProduct.save((err, Product) => {
     if (error) {
-      res.send(error);
+      res.send(err);
     }
 
     res.json(Product);
