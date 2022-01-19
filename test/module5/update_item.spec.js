b1a63877068a19e6e324d7065146442f66785f70
baddar999 <baddar.rehmat@gmail.com>
2022-01-19 20:55:15 +0500

    Completed module

--- test/module5/update_item.spec.js
+++ test/module5/update_item.spec.js
@@ -4,15 +4,12 @@ const expect = require('expect.js');
 describe('Updating a product', () => {
   it('Add a controler to update a product @controller-update', done => {
     try {
-      expect(
-        fs.readFileSync('src/controllers/controllers.js').toString()
-      ).to.contain(
-        `Product.findOneAndUpdate(
-            { _id: req.params.ProductID },
-            req.body,
-            { new: true, useFindAndModify: false },
-            (err, Product) => {`
-      );
+      expect(fs.readFileSync('src/controllers/controllers.js').toString()).to
+        .contain(`Product.findOneAndUpdate(
+                { _id: req.params.ProductID },
+                req.body,
+                { new: true, useFindAndModify: false },
+                (err, Product)`);
       done();
     } catch (error) {
       done(new Error('No controllers for PUT endpoint detected'));
